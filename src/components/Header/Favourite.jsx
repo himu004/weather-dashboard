24e92d4735a547a8075c1757d935c3499b1f57a5
Himu004 <himu.chowdhury01@gmail.com>
2024-02-20 15:33:07 +0600

    Fav Modal Updated

--- src/components/Header/Favourite.jsx
+++ src/components/Header/Favourite.jsx
@@ -3,7 +3,7 @@ const Favourite = ({onShow}) => {
     return (
         <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
             <img src={heart} alt="" />
-            <button onClick={onShow}>Favourite Locations</button>
+            <button onClick={onShow}>Favorite Locations</button>
         </div>
     );
 };
